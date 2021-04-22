b0a82cc86be3761aa9a09791623bc3c888e989a2
Samuel Gref <samgref@gmail.com>
2021-04-22 15:37:35 -0400

    Animatable

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -3,7 +3,8 @@ import { View, FlatList, Text } from "react-native";
 import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
-import Loading from './LoadingComponent';
+import Loading from "./LoadingComponent";
+import * as Animatable from "react-native-animatable";
 
 const mapStateToProps = (state) => {
   return {
@@ -21,34 +22,36 @@ class Directory extends Component {
 
     const renderDirectoryItem = ({ item }) => {
       return (
-        <Tile
-          title={item.name}
-          caption={item.description}
-          featured
-          onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
-          imageSrc={{ uri: baseUrl + item.image }}
-        />
+        <Animatable.View animation="fadeInRightBig" duration={2000}>
+          <Tile
+            title={item.name}
+            caption={item.description}
+            featured
+            onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
+            imageSrc={{ uri: baseUrl + item.image }}
+          />
+        </Animatable.View>
       );
     };
-    
+
     if (this.props.campsites.isLoading) {
       return <Loading />;
-  }
-  if (this.props.campsites.errMess) {
+    }
+    if (this.props.campsites.errMess) {
       return (
-          <View>
-              <Text>{this.props.campsites.errMess}</Text>
-          </View>
+        <View>
+          <Text>{this.props.campsites.errMess}</Text>
+        </View>
       );
-  }
-  return (
+    }
+    return (
       <FlatList
-          data={this.props.campsites.campsites}
-          renderItem={renderDirectoryItem}
-          keyExtractor={item => item.id.toString()}
+        data={this.props.campsites.campsites}
+        renderItem={renderDirectoryItem}
+        keyExtractor={(item) => item.id.toString()}
       />
-  );
-}
+    );
+  }
 }
 
 export default connect(mapStateToProps)(Directory);
